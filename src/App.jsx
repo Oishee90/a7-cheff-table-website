9b64547bb14f2dc6c9b6d849e493d832b5623aa6
oishee <oisheekhan90@gmail.com>
2024-03-16 16:02:12 +0600

    want to cook part complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,76 +2,159 @@
 import './App.css'
 import Banner from './Component/Banner/Banner'
 import Header from './Component/Header/Header'
-import timeImage from './assets/time.png'
-import fireImage from  './assets/fire.png'
+
 import { useEffect, useState } from 'react'
+import Recipies from './Component/Recipies/Recipies'
 
 
 
 
 function App() {
-  const[reciepies, setReciepies] = useState([])
+  const[reciepies, setReciepies] = useState([]);
+  const [cart,setCart] = useState([]);
+  const [deletedItems, setDeletedItems] = useState([]);
+  const totalPreparingTime = deletedItems.reduce((total, item) => total + parseFloat(item.preparing_time), 0);
+  const totalCalories = deletedItems.reduce((total, item) => total + parseFloat(item.calories), 0);
+
   
   useEffect(()=>{
-    fetch("../public/fakeData.json")
+    fetch("./fakeData.json")
     .then(res => res.json())
     .then(data => {setReciepies(data)})
   },[])
-console.log(reciepies)
+
+  const handleCart =(p) => {
+    const isExist = cart.find(rp => rp.recipe_id == p.recipe_id)
+   if(!isExist){
+    setCart([...cart,p])
+   }
+   else{
+   alert("already in card")
+   }
+  }
+  const handleDelete=(id)=> {
+    const deletedItem = cart.find(item => item.recipe_id === id);
+    const newcart = cart.filter(item=>item.recipe_id != id);
+    setCart(newcart)
+    setDeletedItems(prevItems => [...prevItems, deletedItem]);
+  }
+
   return (
     <>
      <Header></Header>
      <Banner></Banner>
 
-     <div className="main-container flex flex-col lg:flex-row md:flex-row gap-5">
+     <div className="main-container flex justify-around h-full gap-3 ">
       {/* card */}
-       <div className="flex flex-col md:flex-row  lg:flex-row justify-around gap-3 ">
-
-       <div className="card w-96  bg-base-100 shadow-xl border border-gray-500 ">
-  <figure className="">
-    <img className='w-full rounded-3xl' src="https://i.ibb.co/QvngMgR/penne-pasta-tomato-sauce-with-chicken-tomatoes-wooden-table.jpg" alt="Shoes" />
-  </figure>
-  <div className="mt-4 ">
-    <h2 className="card-title text-[#282828] font-lexend text-xl font-semibold">Spaghetti Bolognese</h2>
-    <p className="mt-2 text-[#878787] font-lexend text-left">Classic Italian pasta dish with savory meat sauce.</p>
-    <div className='border h-0 border-gray-400 mt-4 '></div>
-    <div className="mt-4 text-left font-lexend ">
-
- 
-      <h1 className='text-[#282828] text-lg font-bold mb-2'> Ingredients: 6 </h1>
-      <ul>
-        <li className='#878787'>500g ground beef</li>
-      </ul>
-      </div>
-      <div className='border h-0 border-gray-400 mt-4 '></div>
-     <div className='mt-4 flex  gap-5'>
-      <div className='flex '><img src={timeImage} alt="" /> <p className='ml-2'>Minutes</p></div>
-      <div className='flex '><img src={fireImage} alt="" /> <p className='ml-2'>600 calories</p></div>
-     </div>
-     <div className='text-left mt-4'>
-     <button className="btn text-xl font-lexend  btn-success hover:bg-green-700 btn-xs sm:btn-sm md:btn-md lg:btn-lg rounded-full">Wait to Cook</button>
-     </div>
-    </div>
- 
+      <div>
+      <div className="card-container w-full grid grid-cols-2 gap-2 ">
+      
+      {
+          reciepies.map(rp => <Recipies 
+            key={rp.recipe_id}
+            handleCart={handleCart}
+          recipe={rp}
+         
+          ></Recipies>)
+          
+        }
+       
+       
 </div>
-
-
-
-       </div>
-
+</div>
 
 {/* cart */}
 
-       <div className="card w-full cart-container  flex-grow  shadow-xl border border-gray-400  rounded-3xl">
+
+       <div className=" w-auto h-auto card  cart-container shadow-xl border border-gray-400  rounded-3xl">
   <div className="p-8 ">
-    <h2 className=" text-center">Card title!</h2>
+    <h2 className=" text-center text-2xl font-lexend font-bold">Want To Cook :{cart.length}</h2>
+    <div className='border h-0 border-gray-400 mt-4 '></div>
+    <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Time</th>
+        <th>Calories</th>
+      </tr>
+    </thead>
+    <tbody>
+        {
+cart.map((item, index) => (
+      
+    
+    
+      <tr key={index} className="hover">
+        <th>{item.recipe_id}</th>
+        <td>{item.recipe_name}</td>
+        <td>{item.preparing_time}</td>
+        <td>{item.calories}</td>
+        <td><button onClick={()=>handleDelete(item.recipe_id)} className='btn hover:bg-green-900 bg-green-500 border-none text-black rounded-full'>Preparing</button></td>
+      </tr>
+      ))
+   
+  }
+      
+    </tbody>
+  </table>
+</div>
+{/* current item */}
+<div className='mt-8'>
+<h2 className=" text-center text-xl font-lexend font-bold">Currently Cooking:{deletedItems.length}</h2>
+<div className='border h-0 border-gray-400 mt-4 '></div>
+<div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Time</th>
+        <th>Calories</th>
+      </tr>
+    </thead>
+    <tbody>
+    {deletedItems.map((item, index) => (
+          <tr key={index} className="hover">
+          <th>{item.recipe_id}</th>
+          <td>{item.recipe_name}</td>
+          <td>{item.preparing_time}</td>
+          <td>{item.calories}</td>
+          </tr>
+          
+        ))}
+       
+       <tr>
+  <th colSpan="4">Total Preparing Time :</th>
+  
+  <td >{totalPreparingTime}miniute</td>
+</tr>
+
+<tr>
+  <th colSpan="3">Total Calories :</th>
+  <td></td>
+  <td colSpan="">{totalCalories} Calories</td>
+</tr>
+    
+      </tbody>
+      </table>
+      </div>
+      <ul>
+        
+      </ul>
+</div>
   
     
   </div>
 </div>
+
+
        </div>
 
-     
+    
      
     </>
   )
---
+++ src/Component/Recipies/Recipies.jsx
@@ -0,0 +1,49 @@
+
+import PropTypes from 'prop-types';
+import timeImage from '../../assets/time.png'
+import fireImage from  '../../assets/fire.png'
+
+const Recipies = ({recipe,  handleCart}) => {
+  console.log(handleCart)
+   
+    return (
+     
+        <div className="card w-auto  bg-base-100 shadow-xl border border-gray-500 ">
+  <figure className="">
+    <img className='w-full rounded-3xl' src={recipe.image} alt="Shoes" />
+  </figure>
+  <div className="mt-4 ">
+    <h2 className="card-title text-[#282828] font-lexend text-xl font-semibold">{recipe.recipe_name}</h2>
+    <p className="mt-2 text-[#878787] font-lexend text-left">{recipe.short_description}</p>
+    <div className='border h-0 border-gray-400 mt-4 '></div>
+    <div className="mt-4 text-left font-lexend ">
+
+ 
+      <h1 className='text-[#282828] text-lg font-bold mb-2'> Ingredients: {recipe.ingredients.length}</h1>
+      
+      {recipe.ingredients.map((ingredient, index) => (
+              <li key={index}>{ingredient}</li>))
+      }
+     
+      </div>
+      <div className='border h-0 border-gray-400 mt-4 '></div>
+     <div className='mt-4 flex  gap-5'>
+      <div className='flex '><img src={timeImage} alt="" /> <p className='ml-2'>{recipe.preparing_time}</p></div>
+      <div className='flex '><img src={fireImage} alt="" /> <p className='ml-2'>{recipe.calories}</p></div>
+     </div>
+     <div className='text-left mt-4'>
+     <button onClick={(e)=> handleCart(recipe)} className="btn text-xl font-lexend  btn-success hover:bg-green-700 btn-xs sm:btn-sm md:btn-md lg:btn-lg rounded-full">Wait to Cook</button>
+     </div>
+    </div>
+    </div>
+
+
+    );
+};
+
+Recipies.propTypes = {
+ recipe: PropTypes.object.isRequired,
+
+};
+
+export default Recipies;
